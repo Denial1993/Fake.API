e98c0fe16751d8b34979c003ee13a58c0bbfb883
Daniel <denial821124@gmail.com>
2025-01-13 15:10:01 +0800

    推送更新種子數據欄位跟mock假資料

---
+++ Fake.API/Models/DepartureCity.cs
@@ -0,0 +1,10 @@
+﻿namespace Fake.API.Models
+{
+    public enum DepartureCity
+    {
+        Beijing, //北京
+        Shanhai, //上海
+        Canton, //廣州
+        Shenzhen, //深圳
+    }
+}
---
+++ Fake.API/Models/TripType.cs
@@ -0,0 +1,11 @@
+﻿namespace Fake.API.Models
+{
+    public enum TripType
+    {
+        HotelAndAttractions,//酒店+景點
+        Group,//跟團游
+        PrivateGroup,//私家團
+        BackPackTour,//自由行
+        SemiBackPackTour //半自助遊
+    }
+}
