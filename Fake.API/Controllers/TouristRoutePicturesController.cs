327da7d34c352a100619ea9c9eda96bc91e8d4e9
Daniel <denial821124@gmail.com>
2025-01-15 21:55:09 +0800

    新增 Controller:TouristRoutePicturesController

---
+++ Fake.API/Controllers/TouristRoutePicturesController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Fake.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fake.API.Controllers
+{
+    [ApiController]
+    [Route("api/touristroutes/{touristRouteId}/pictures")]
+    public class TouristRoutePicturesController : ControllerBase
+    {
+        private ITouristRouteRepository _touristRouteRepository;
+        private IMapper _mapper;
+
+        public TouristRoutePicturesController(ITouristRouteRepository touristRouteRepository, IMapper mapper)
+        {
+            _touristRouteRepository = touristRouteRepository;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
