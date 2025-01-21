9f85f7dc1dfbb48ecb75e3e7067537bcb0c7c231
Daniel <denial821124@gmail.com>
2025-01-21 15:00:17 +0800

    創建資料夾 [ ValidationAttributes ] 並且 建立 class [TouristRouteTitleMustBeDifferentFromDescriptionAttribute] 然後讓她繼承[ValidationAttribute]

---
+++ Fake.API/ValidationAttributes/TouristRouteTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fake.API.ValidationAttributes
+{
+    public class TouristRouteTitleMustBeDifferentFromDescriptionAttribute :ValidationAttribute
+    {
+    }
+}
